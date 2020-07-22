11dfe76d1b97db58d9d96f8c2bc3ba2c8a014bb9
bbmon <bb_@naver.com>
2020-07-22 19:18:54 +0900

    200723. 버전 수정.

--- BBOracleDiff/src/com/bb/dbdiff/common/CommonConst.java
+++ BBOracleDiff/src/com/bb/dbdiff/common/CommonConst.java
@@ -3,7 +3,7 @@ package com.bb.dbdiff.common;
 public class CommonConst {
 
 	
-	public static String version = "200722";
+	public static String version = "200723";
 	public static String propFilePath = "option.properties";
 	public static String compareToolPath = "";
 }
