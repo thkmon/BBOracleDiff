52ef994f37d9dc03ea40f22da8bc65bb705418ac
thkmon <thkmon@gmail.com>
2026-04-23 17:00:56 +0900

    버전값 수정

--- BBOracleDiff/src/com/bb/dbdiff/common/CommonConst.java
+++ BBOracleDiff/src/com/bb/dbdiff/common/CommonConst.java
@@ -3,7 +3,7 @@ package com.bb.dbdiff.common;
 public class CommonConst {
 
 	
-	public static String version = "20230714";
+	public static String version = "20260423";
 	public static String propFilePath = "option.properties";
 	public static String compareToolPath = "";
 }
